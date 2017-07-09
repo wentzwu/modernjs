1864e793fe1f07fee4de6c4d0880dfc0bea902d3
wentzwu <wentzwu@gmail.com>
2017-07-09 10:17:33 +0800

    fix folder name typo

---
+++ QuickStart/js/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Friend_1 = require("Friend");
+var friend = new Friend_1.Friend();
+friend.FirstName = "Bruce";
+friend.LastName = "Wu";
+console.log("hello " + friend.FullName);
