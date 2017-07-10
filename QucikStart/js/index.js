3bd75b7711639462d8e8d9429fb51725e9d4476b
wentzwu <wentzwu@gmail.com>
2017-07-10 10:37:24 +0800

    add ionic-tabs-app

--- QucikStart/js/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Friend_1 = require("Friend");
-var friend = new Friend_1.Friend();
-friend.FirstName = "Bruce";
-friend.LastName = "Wu";
-console.log("hello " + friend.FullName);
--- QucikStart/modules/Friend.ts
+++
@@ -1,8 +0,0 @@
-
-export class Friend {
-    FirstName: string;
-    LastName: string;
-    get FullName(): string {
-        return `${this.LastName},${this.FirstName}`;
-    }
-}
